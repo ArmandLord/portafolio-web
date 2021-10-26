538000069a2b8aaa703259ed4d41f0bc3a670a6f
Armando Perez <armandlord98@gmail.com>
2021-10-26 18:31:00 -0500

    bugs ok

--- src/components/ContainerInfo/ContainerInfo.js
+++ src/components/ContainerInfo/ContainerInfo.js
@@ -5,7 +5,7 @@ import {
 } from "react-vertical-timeline-component";
 import "react-vertical-timeline-component/style.min.css";
 import {
-  LogoInfo,
+  // LogoInfo,
   ContainerBackground,
   Info,
   ContainerText,
