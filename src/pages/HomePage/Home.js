3bbec25b6e93b6eb90214ec40b9649927a16f352
Armando Perez <armandlord98@gmail.com>
2021-10-27 18:23:43 -0500

    Footer, Contact and scrollLink

--- src/pages/HomePage/Home.js
+++ src/pages/HomePage/Home.js
@@ -1,8 +1,14 @@
-import React from 'react'
+import { useEffect } from 'react';
+import { useLocation } from "react-router-dom";
 import { ContainerLogo, ContainerInfo, Skills, ProjectSlider } from '../../components'
 
 
 const Home = () => {
+    const { pathname } = useLocation();
+
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, [pathname]);
     return (
         <>
             <ContainerLogo/>
